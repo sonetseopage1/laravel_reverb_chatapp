f81d40db7b4f5c949e2ea542c05bf4793120e096
sonet1182 <shawnshikder1996@gmail.com>
2025-03-14 11:20:11 +0600

    Message has been grouped

--- resources/views/message/chat_box.blade.php
+++ resources/views/message/chat_box.blade.php
@@ -22,29 +22,67 @@
                     <div class="card-body">
                         <div class="Chat__wrapper" id="chat-box">
                             <ul class="Chat" id="notification">
+                                @php
+                                    $prevSender = null;
+                                    $messagesGrouped = [];
+                                @endphp
+
                                 @foreach ($messages as $message)
                                     @if ($message->receiver == auth()->id())
+                                        @if ($prevSender == 'auth')
+                                            @php
+                                                // Add message to the last grouped messages
+                                                $messagesGrouped[count($messagesGrouped) - 1]['messages'][] = $message;
+                                            @endphp
+                                        @else
+                                            @php
+                                                // Start a new group for the authenticated user
+                                                $messagesGrouped[] = ['sender' => 'auth', 'messages' => [$message]];
+                                            @endphp
+                                        @endif
+                                        @php
+                                            $prevSender = 'auth';
+                                        @endphp
+                                    @else
+                                        @if ($prevSender == 'receiver')
+                                            @php
+                                                // Add message to the last grouped messages
+                                                $messagesGrouped[count($messagesGrouped) - 1]['messages'][] = $message;
+                                            @endphp
+                                        @else
+                                            @php
+                                                // Start a new group for the receiver
+                                                $messagesGrouped[] = ['sender' => 'receiver', 'messages' => [$message]];
+                                            @endphp
+                                        @endif
+                                        @php
+                                            $prevSender = 'receiver';
+                                        @endphp
+                                    @endif
+                                @endforeach
+
+                                @foreach ($messagesGrouped as $group)
+                                    @if ($group['sender'] == 'auth')
                                         <li class="Chat_item Chat_item_l">
                                             <div class="i_man">
-                                                <img src="https://i.postimg.cc/L5v3P42G/IMG-20180513-182600080.jpg"
-                                                    class="i_man-image" />
+                                                <img src="https://i.postimg.cc/L5v3P42G/IMG-20180513-182600080.jpg" class="i_man-image" />
                                             </div>
                                             <div class="Chat_msgs">
-                                                <div class="msg">
-                                                    <div class="msg-content">
-                                                        {{ $message->body }}
+                                                @foreach ($group['messages'] as $message)
+                                                    <div class="msg">
+                                                        <div class="msg-content">{{ $message->body }}</div>
                                                     </div>
-                                                </div>
+                                                @endforeach
                                             </div>
                                         </li>
                                     @else
                                         <li class="Chat_item Chat_item_r">
                                             <div class="Chat_msgs">
-                                                <div class="msg">
-                                                    <div class="msg-content">
-                                                        {{ $message->body }}
+                                                @foreach ($group['messages'] as $message)
+                                                    <div class="msg">
+                                                        <div class="msg-content">{{ $message->body }}</div>
                                                     </div>
-                                                </div>
+                                                @endforeach
                                             </div>
                                         </li>
                                     @endif
